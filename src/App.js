472992fcdb22d00a9a92acc320dadd4fc84d8ec4
Pushkar-Anand-MIT <pushkar.anand@learner.manipal.edu>
2022-10-05 22:41:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+import TimeandLocation from './components/TimeandLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+
+      <TimeandLocation />
+      <TemperatureAndDetails />
+      <Forecast title="Hourly Forecast"/>
+      <Forecast title="Daily Forecast"/>
+    </div>
+  );
+}
+
+export default App;

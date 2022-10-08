e34537993def83bf27b456656e7506de6d715bea
Pushkar-Anand-MIT <pushkar.anand@learner.manipal.edu>
2022-10-08 19:22:22 +0530

    Api integration

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import Inputs from './components/Inputs';
 import TimeandLocation from './components/TimeandLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
+import getWeatherData from './Services/weatherService';
 
 function App() {
+  const fetchWeather = async () =>{
+    const data = await getWeatherData('weather', {q: "london"});
+    console.log(data);
+  }
+
+  fetchWeather();
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />

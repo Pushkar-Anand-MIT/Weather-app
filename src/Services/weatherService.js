a35b549edd67e5ff35ba38099aa3f8d3593d75d9
Pushkar-Anand-MIT <pushkar.anand@learner.manipal.edu>
2022-10-08 20:53:11 +0530

    api

--- src/Services/weatherService.js
+++ src/Services/weatherService.js
@@ -1,12 +1,21 @@
-const API_KEY = "7d9867cb339a33a11fc742451ac1e0d9";
+
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams)=>{
     const url = new URL(BASE_URL + '/' + infoType);
     url.search = new URLSearchParams({...searchParams, appid:API_KEY})
 
+    console.log(url);
+    
     return fetch(url)
     .then((res)=>res.json())
+
+    
 };
 
 
+
+
+// const getFormattedWeatherData = (searchParams) =>{
+//     const formattedCurrentWeather = await getFormattedWeatherData('weather',searchParams);
+// }
